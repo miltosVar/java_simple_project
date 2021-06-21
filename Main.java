0091924d7c7a321194f69301665fcc9dd1592f13
miltosvar <milVardakas@outlook.com.gr>
2021-06-21 21:01:54 +0300

    modify Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("If you see this message, then the build went successfully");
+        System.out.println("If you see this message, then the build went successfully2222");
         System.out.println("At " + LocalDateTime.now());
     }
 }
